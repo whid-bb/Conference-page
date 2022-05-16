042d1b2175b6d9622f26e1315adb17bf99415786
whid <kontakt@cgnk.pl>
2022-05-16 11:08:53 +0200

    Setup webpack, linters, base js, scss files

---
+++ config/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: ['./src/index.js', './src/scss/base.scss'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: {
+      paths: ['./src/**/*'],
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: true,
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new WebpackManifestPlugin(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: true,
+    assetModuleFilename: './img/[hash][ext]',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env', {}]],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
